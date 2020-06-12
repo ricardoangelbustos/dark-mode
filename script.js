359be7f5290e518f30e60cdc0a5ab885007a682e
Ricardo Bustos <ricardobustos14@outlook.com>
2020-06-11 23:53:49 -0300

    Agregado JavaScript externo

---
+++ script.js
@@ -0,0 +1,7 @@
+function darkmode() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+
+    var element2 = document.getElementById("button")
+    element2.classList.toggle("dark-mode");
+}
